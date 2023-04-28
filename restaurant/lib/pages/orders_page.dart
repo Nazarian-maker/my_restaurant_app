19db2df0e953f851d9078fc48fccd6a5b8a1fa18
Arthur <61287582+Nazarian-maker@users.noreply.github.com>
2023-04-28 17:36:11 +0300

    dodelat'

--- restaurant/lib/pages/orders_page.dart
+++ restaurant/lib/pages/orders_page.dart
@@ -36,54 +36,62 @@ class Orders extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Список заказов'),
       ),
-      body: Center(
-        child: SizedBox(
-          height: 1000,
-          child: ListView.builder(
-            itemExtent: 80,
-            itemCount: model?.orders.length,
-            itemBuilder: (BuildContext context, int index) {
-              final order = model?.orders[index];
-              return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black.withOpacity(0.2)),
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 8,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  clipBehavior: Clip.hardEdge,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text('Номер заказа ${order?.num},'),
-                        const SizedBox(
-                          width: 10,
+      body: Stack(
+        children: [
+          Center(
+            child: ListView.builder(
+              itemExtent: 80,
+              itemCount: model?.orders.length,
+              itemBuilder: (BuildContext context, int index) {
+                final order = model?.orders[index];
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black.withOpacity(0.2)),
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 8,
+                          offset: const Offset(0, 2),
                         ),
-                        Text('стоимость  ${order?.count},'),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        (order?.is_closed == false)
-                            ? const Text('статус: открыт')
-                            : const Text('статус: закрыт')
                       ],
                     ),
+                    clipBehavior: Clip.hardEdge,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text('Номер заказа ${order?.num},'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text('стоимость  ${order?.count},'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          (order?.is_closed == false)
+                              ? const Text('статус: открыт')
+                              : const Text('статус: закрыт')
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10),
+              onTap: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
